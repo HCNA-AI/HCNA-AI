1482acf88ad9cc2a1d1bcdae2b6d42da04d20dc3
HCNA-AI <37129146+HCNA-AI@users.noreply.github.com>
2018-05-09 17:49:43 +0800

    Update 2withsession.py

--- 2withsession.py
+++ 2withsession.py
@@ -6,5 +6,5 @@ a = tf.constant(3)                     #define variable 3
 b = tf.constant(4)                     #define variable 4
 
 with tf.Session() as sess:           #create session
-    print ("相加: %i" % sess.run(a+b))
-    print( "相乘: %i" % sess.run(a*b))
+    print ("add: %i" % sess.run(a+b))
+    print( "mul: %i" % sess.run(a*b))
