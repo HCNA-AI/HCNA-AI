b92b035455a8de8f75eae1514a61bac256383647
HCNA-AI <37129146+HCNA-AI@users.noreply.github.com>
2018-05-09 16:13:27 +0800

    Update and rename 8线性回归模型保存及载入.py to 8model_save.py

--- 8model_save.py
+++ 8model_save.py
@@ -11,46 +11,45 @@ def moving_average(a, w=10):
         return a[:]    
     return [val if idx < w else sum(a[(idx-w):idx])/w for idx, val in enumerate(a)]
 
-#生成模拟数据
+#Generate simulation data
 train_X = np.linspace(-1, 1, 100)
-train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.3 # y=2x，但是加入了噪声
+train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.3 # y=2x，added noise
 print(train_X.shape)
 print(train_Y.shape)
-#重置图
+#reset graph
 tf.reset_default_graph()
 
-# 创建模型
-# 占位符
+# create model
+# placeholder
 X = tf.placeholder("float")
 Y = tf.placeholder("float")
-# 模型参数
+# Model parameters
 W = tf.Variable(tf.random_normal([1]), name="weight")
 b = tf.Variable(tf.zeros([1]), name="bias")
-# 前向结构
+# Forward structure
 z = tf.multiply(X, W)+ b
 
-#反向优化
+#Reverse optimization
+
 cost =tf.reduce_mean( tf.square(Y - z))
 learning_rate = 0.001
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost) #Gradient descent
 
-# 初始化变量等操作
+# Initialize variables and operations
 init = tf.global_variables_initializer()
-#参数设置
-training_epochs = 100000
+#parameter settings
+training_epochs = 10000
 display_step = 2
-saver = tf.train.Saver() # 生成saver
+saver = tf.train.Saver() # saver
 savedir = "log/"
-# 启动session
+# run the session
 with tf.Session() as sess:
     sess.run(init)
-
-    # 添加Sess中的训练代码
     for epoch in range(training_epochs):
         # for (x, y) in zip(train_X, train_Y):
         sess.run(optimizer, feed_dict={X: train_X, Y: train_Y})
 
-        #显示训练中的详细信息
+        #display the train
         if epoch % display_step >= 0:
             loss = sess.run (cost, feed_dict={X: train_X, Y:train_Y})
             print ("Epoch:", epoch+1, "cost=", loss,"W=", sess.run(W), "b=", sess.run(b))
