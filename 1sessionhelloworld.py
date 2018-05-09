4efdb94a16ec105fad77acd612f3a54484851583
HCNA-AI <37129146+HCNA-AI@users.noreply.github.com>
2018-05-09 16:05:12 +0800

    Update 1sessionhelloworld.py

--- 1sessionhelloworld.py
+++ 1sessionhelloworld.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')  #定义一个常量
-sess = tf.Session()                             #建立一个session
-print (sess.run(hello))                        #通过session里面的run来运行结果
-sess.close()                                     #关闭session
+hello = tf.constant('Hello, TensorFlow!')  #Define constants
 
+sess = tf.Session()                            #Create a session
 
-
+print (sess.run(hello))                        #Run the session
+sess.close()                                    #colse session
