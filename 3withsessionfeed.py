b4aeca6c87edf3a1307d3ec5aaeab985dcfa8adf
HCNA-AI <37129146+HCNA-AI@users.noreply.github.com>
2018-05-09 16:17:34 +0800

    Update 3withsessionfeed.py

--- 3withsessionfeed.py
+++ 3withsessionfeed.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
+"""
 
+"""
 import tensorflow as tf
-a = tf.placeholder(tf.int16)
-b = tf.placeholder(tf.int16)
+a = tf.placeholder(tf.int16)     define palceholder
+b = tf.placeholder(tf.int16)     
 add = tf.add(a, b)
-mul = tf.multiply(a, b)                      #a与b相乘
+mul = tf.multiply(a, b)                      #a*b
 with tf.Session() as sess:
-    #计算具体数值
+    #Calculate the value
     print ("相加: %i" % sess.run(add, feed_dict={a: 3, b: 4}))
     print ("相乘: %i" % sess.run(mul, feed_dict={a: 3, b: 4}))
 mul=tf.multiply(a,b)
 with tf.Session() as sess:
-    #将 op运算通过run打印出来
+    #run the session
     print("相加： %i"%sess.run(add,feed_dict={a:3,b:4}))
-    #将add节点打印出来
-    print("相乘：%i"%sess.run(nul,feed_dict={a:3,b:4}))
+    #add the op
+    print("相乘：%i"%sess.run(mul,feed_dict={a:3,b:4}))
