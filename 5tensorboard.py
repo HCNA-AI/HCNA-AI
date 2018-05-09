45d7397538fa01a5d046181d1f0cf925fe61e1fd
HCNA-AI <37129146+HCNA-AI@users.noreply.github.com>
2018-05-09 16:16:01 +0800

    Update and rename 5线性回归的tensorboard可视化.py to 5tensorboard.py

--- 5tensorboard.py
+++ 5tensorboard.py
@@ -4,63 +4,66 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 plotdata = { "batchsize":[], "loss":[] }
 def moving_average(a, w=10):
     if len(a) < w: 
         return a[:]    
     return [val if idx < w else sum(a[(idx-w):idx])/w for idx, val in enumerate(a)]
 
-#生成模拟数据
+
+#Generate simulation data
 train_X = np.linspace(-1, 1, 100)
-train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.3 # y=2x，但是加入了噪声
-#图形显示
+train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.3 # y=2x，Increase noise
+#Graphic display
 # plt.plot(train_X, train_Y, 'ro', label='Original data')
 # plt.legend()
-# plt.show()
+plt.show()
+
 
 tf.reset_default_graph()
 
-# 创建模型
-# 占位符
+# Create model
+# placeholder
 X = tf.placeholder("float")
 Y = tf.placeholder("float")
-# 模型参数
+# Model parameters
 W = tf.Variable(tf.random_normal([1]), name="weight")
 b = tf.Variable(tf.zeros([1]), name="bias")
 
-# 前向结构
+# Forward structure
 z = tf.multiply(X, W)+ b
-tf.summary.histogram('z',z)#将预测值以直方图显示
-#反向优化
+tf.summary.histogram('z',z)#Display predicted values ​​as histograms
+#Reverse optimization
 cost =tf.reduce_mean( tf.square(Y - z))
-tf.summary.scalar('loss_function', cost)#将损失以标量显示
+tf.summary.scalar('loss_function', cost)#show the cost
 learning_rate = 0.01
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost) #Gradient descent
 
-# 初始化变量
+#Initialize variables
 init = tf.global_variables_initializer()
-#参数设置
+#parameter settings
 training_epochs = 20
 display_step = 2
 
-# 启动session
+# run the session
 with tf.Session() as sess:
     sess.run(init)
     
-    merged_summary_op = tf.summary.merge_all()#合并所有summary
-    #创建summary_writer，用于写文件
+    merged_summary_op = tf.summary.merge_all()#merge all the summary
+    #create summary_writer
     summary_writer = tf.summary.FileWriter('log/mnist_with_summaries',sess.graph)
 
-    #向模型中输入数据
+    #Enter data into the model
     for epoch in range(training_epochs):
         for (x, y) in zip(train_X, train_Y):
             sess.run(optimizer, feed_dict={X: x, Y: y})
             
             #生成summary
             summary_str = sess.run(merged_summary_op,feed_dict={X: x, Y: y});
-            summary_writer.add_summary(summary_str, epoch);#将summary 写入文件
+            summary_writer.add_summary(summary_str, epoch);#write the summary 
 
-        #显示训练中的详细信息
+        #display the train
         if epoch % display_step == 0:
             loss = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
             print ("Epoch:", epoch+1, "cost=", loss,"W=", sess.run(W), "b=", sess.run(b))
@@ -72,9 +75,9 @@ with tf.Session() as sess:
     print ("cost=", sess.run(cost, feed_dict={X: train_X, Y: train_Y}), "W=", sess.run(W), "b=", sess.run(b))
     #print ("cost:",cost.eval({X: train_X, Y: train_Y}))
 
-    #图形显示
-    # plt.plot(train_X, train_Y, 'ro', label='Original data')
-    # plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    #display the plot
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
     # plt.legend()
     # plt.show()
     
@@ -86,7 +89,7 @@ with tf.Session() as sess:
     # plt.ylabel('Loss')
     # plt.title('Minibatch run vs. Training loss')
      
-    # plt.show()
+    plt.show()
 
     print ("x=0.2，z=", sess.run(z, feed_dict={X: 0.2}))
     
