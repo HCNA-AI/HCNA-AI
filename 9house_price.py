fa6406bfdb213d152529f4a5b2a1c092909ecbab
HCNA-AI <37129146+HCNA-AI@users.noreply.github.com>
2018-05-09 16:12:34 +0800

    Update and rename 预测.py to 9house_price.py

--- 9house_price.py
+++ 9house_price.py
@@ -1,46 +1,46 @@
+# coding:utf-8
+
 from __future__ import print_function, division
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
-# 我是在Jupyter Notebook里运行的，所以需要这行
-%matplotlib inline
+# import module
 
-# 读入数据
-train = pd.read_csv("Dataset/train.csv")
-# 选取房屋面积小于１２０００的数据
-train = train[train['LotArea'] < 12000]
-train_X = train['LotArea'].values.reshape(-1, 1)
-train_Y = train['SalePrice'].values.reshape(-1, 1)
+#import data
+train = pd.read_csv("bj_housing2.csv")
+#
+train = train[train['Area'] < 12000]
+train_X = train['Area'].values.reshape(-1, 1)
+train_Y = train['Value'].values.reshape(-1, 1)
 
 n_samples = train_X.shape[0]
-# 学习率
 learning_rate = 2
-# 迭代次数
+# set learning rate
 training_epochs = 1000
-# 每多少次输出一次迭代结果
+# set display_step
 display_step = 50
 
-# 这个X和Y和上面的train_X,train_Y是不一样的，这里只是个占位符，
-# 训练开始的时候需要“喂”(feed)数据给它
+# init placeholder
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
-# 定义模型参数
+#define defined learning parameters
 W = tf.Variable(np.random.randn(), name="weight", dtype=tf.float32)
 b = tf.Variable(np.random.randn(), name="bias", dtype=tf.float32)
 
-# 定义模型
-pred = tf.add(tf.mul(W, X), b)
-# 定义损失函数
+# Build a forward propagation structure
+pred = tf.add(tf.multiply(W, X), b)
+# cost function
 cost = tf.reduce_sum(tf.pow(pred-Y, 2)) / (2 * n_samples)
-# 使用Adam算法，至于为什么不使用一般的梯度下降算法，一会说
+# Using the gradient descent optimizer
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
-# 初始化所有变量
-init = tf.initialize_all_variables()
+# init
+# init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
-# 训练开始
+# run the session
 with tf.Session() as sess:
     sess.run(init)
 
@@ -56,7 +56,7 @@ with tf.Session() as sess:
     training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
     print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
 
-    # 画图
+    # show the plot
     plt.plot(train_X, train_Y, 'ro', label="Original data")
     plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label="Fitted line")
     plt.legend()
